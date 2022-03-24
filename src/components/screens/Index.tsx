65d82a5e785d9c598e769b33779045960c48a04e
jqjjian <jqjjian@126.com>
2022-03-24 14:41:45 +0800

    change font type

--- src/components/screens/Index.tsx
+++ src/components/screens/Index.tsx
@@ -171,8 +171,8 @@ const Index = () => {
             <Head title="ICES | Event LOG" />
             <div className="form-control w-3/4 pt-20">
                 <div className="input-group">
-                    <input type="text" placeholder="Search by Canister Id, Principal Id" className="input input-bordered w-full h-16 text-xl" />
-                    <button className="btn btn-square h-16 w-20 bg-gray-400 border-gray-400">
+                    <input type="text" placeholder="Search by Canister Id, Principal Id" className="input input-bordered w-full h-14 text-xl" />
+                    <button className="btn btn-square h-14 w-20 bg-gray-400 border-gray-400">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                         </svg>
